c9831ef0de49ddec1be876a46b48dd30fd08f64c
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-09-22 19:31:47 +0300

    переписал скрипт

--- main.js
+++ main.js
@@ -1,72 +1,116 @@
-const minimalPosition = 0
-const containerForDifficult = document.querySelectorAll('.difficult-level div')
-const gameMenu = document.querySelector('#menu')
-const gameContainer = document.querySelector('#game-container')
-const startButton = document.querySelector('#start-button')
 
-// сложность по умочанию "простой"
-let difficult = 3
+let cards = [];
+const numberOfCard = getGameDifficult();
+const startButton = document.getElementById('game-start');
 
-const handlerForUserSetDifficult = function() {
-  difficult = +(this.dataset.diff)
-  console.log(difficult)
-  return difficult
-}
+function getGameDifficult() {
+  const diff = document.getElementsByName('diff');
+  const easyChecked = document.getElementById('checked-easy');
+  const mediumChecked = document.getElementById('checked-medium');
+  const hardChecked = document.getElementById('checked-hard');
 
-const handlerForUserFindBug = function() {
-  // например тут if (!this) то {classList.add(некликабельно)}
-  if(this.classList.contains('bug')) {
-    this.classList.add('card_bug', 'clicked')
-    this.addEventListener('click', resetTable)
+  if (diff[0].checked) {
+    easyChecked.classList.add('checked');
+    mediumChecked.classList.remove('checked');
+    hardChecked.classList.remove('checked');
+    return 3;
+  } else if (diff[1].checked) {
+    easyChecked.classList.remove('checked');
+    mediumChecked.classList.add('checked');
+    hardChecked.classList.remove('checked');
+    return 6;
+  } else if (diff[2].checked) {
+    easyChecked.classList.remove('checked');
+    mediumChecked.classList.remove('checked');
+    hardChecked.classList.add('checked');
+    return 10;
   } else {
-    this.classList.add('card_inside', 'clicked')
-    this.addEventListener('click', resetTable)
-  }
-  if(!(this.classList.contains('clicked'))) {
-    this.removeEventListener('click')
-  }
-}
+    easyChecked.classList.add('checked');
+    return 3;
+  };
+};
 
-containerForDifficult.forEach((elem)=> {
-  elem.addEventListener('click', handlerForUserSetDifficult) 
-  })
+const createAndShowCards = () => {
+  getGameDifficult();
+  createMainContainer();
+  const main = document.getElementById('main');
+  const keysOfCardClasses = {
+    3: 'main-for-three-cards',
+    6: 'main-to-six-cards',
+    10: 'main-to-ten-cards',
+  };
+  
+  createCards(numberOfCard);
+  addFrontAndBackCardsSide();
+  main.className = keysOfCardClasses[numberOfCard];
+};
 
-function createTable(diff) {
-  for(let i = 0; i < diff; i++) {
-    const card = document.createElement('div')
-    card.classList.add('card_top')
-    gameContainer.appendChild(card, gameContainer)
-  }
-}
+const createMainContainer = () => {
+  const main = document.createElement('main');
+  main.setAttribute('id', 'main');
+  document.body.append(main);
+};
 
-function resetTable() {
-  gameMenu.classList.remove('hide')
-  while(gameContainer.firstChild) {
-    gameContainer.removeChild(gameContainer.firstChild);
-  }
-}
+const createCards = (number) => {
+  for (let i = 0; i < number; i++) {
+    const card = document.createElement('div');
+    card.className = 'card card-looser';
+    document.body>main.append(card);
+  };
+};
 
-function getRandomPosition(minPos, diff) {
-  return Math.floor(Math.random() * (diff - minPos)) + minPos;
-}
+const addFrontAndBackCardsSide = () => {
+  let numberOfEachCardTurns = 0;
+  Array.from(document.querySelectorAll('.card')).forEach(el => {
+    cards.push(el);
+    const cardFront = document.createElement('div');
+    cardFront.className = 'card__front';
+    cards[numberOfEachCardTurns].append(cardFront);
 
-function setBug(minPos, diff) {
-  const bugPosition = getRandomPosition(minPos, diff)
-  const gameCards = document.querySelectorAll('#game-container div')
-  gameCards[bugPosition].classList.add('bug')
-}
+    const cardBack = document.createElement('div');
+    cardBack.className = 'card__looser_back';
+    cards[numberOfEachCardTurns].append(cardBack);
+    numberOfEachCardTurns++;
+  });
+};
 
-startButton.addEventListener('click', () => {
-  resetTable()
-  createTable(difficult)
-  setBug(minimalPosition, difficult)
-  const ArrIntoBugFind = document.querySelectorAll('#game-container div')
-  ArrIntoBugFind.forEach((elem) => {
-    elem.addEventListener('click', handlerForUserFindBug)
-  })
-  gameMenu.classList.add('hide')
-})
+const setRandomOfWinner = () => {
+  const randomNumberOfCard = cards[Math.floor(Math.random() * (numberOfCard))];
+  const a = randomNumberOfCard.querySelector('.card__looser_back');
+  a.className = 'card__winner_back';
+  randomNumberOfCard.setAttribute('id', 'card-win');
+  randomNumberOfCard.className = 'card card__winner';
+};
 
-
-
-
+const startGameAndDecideWinnerAndLoosers = () => {
+  const menu = document.getElementById('nav');
+  let numberOfClick = 0;
+  menu.className = 'nav-before-start';
+  const body = document.body;
+  createAndShowCards();
+  setRandomOfWinner();
+  
+  const losers = document.querySelectorAll('card__looser');
+  losers.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        if (elem.className !== 'card__looser_rotated' && numberOfClick == 0) {
+          elem.className = 'card__looser_rotated';
+          numberOfClick++;
+        } else {
+          location.reload(true);
+      };
+    });
+  });
+      
+  const win = document.getElementById('card-win');
+  const clickOnWinnerCard = () => {
+      if (win.className !== 'card__winner_rotated' && numberOfClick == 0) {
+        win.className = 'card__winner_rotated';
+        numberOfClick++;
+       } else {
+         location.reload(true);
+       };
+     };
+  win.addEventListener('click', clickOnWinnerCard);
+};
+startButton.addEventListener('click', startGameAndDecideWinnerAndLoosers, {once: true});
---
+++ script-old.js
@@ -0,0 +1,72 @@
+const minimalPosition = 0
+const containerForDifficult = document.querySelectorAll('.difficult-level div')
+const gameMenu = document.querySelector('#menu')
+const gameContainer = document.querySelector('#game-container')
+const startButton = document.querySelector('#start-button')
+
+// сложность по умочанию "простой"
+let difficult = 3
+
+const handlerForUserSetDifficult = function() {
+  difficult = +(this.dataset.diff)
+  console.log(difficult)
+  return difficult
+}
+
+const handlerForUserFindBug = function() {
+  // например тут if (!this) то {classList.add(некликабельно)}
+  if(this.classList.contains('bug')) {
+    this.classList.add('card_bug', 'clicked')
+    this.addEventListener('click', resetTable)
+  } else {
+    this.classList.add('card_inside', 'clicked')
+    this.addEventListener('click', resetTable)
+  }
+  if(!(this.classList.contains('clicked'))) {
+    this.removeEventListener('click')
+  }
+}
+
+containerForDifficult.forEach((elem)=> {
+  elem.addEventListener('click', handlerForUserSetDifficult) 
+  })
+
+function createTable(diff) {
+  for(let i = 0; i < diff; i++) {
+    const card = document.createElement('div')
+    card.classList.add('card_top')
+    gameContainer.appendChild(card, gameContainer)
+  }
+}
+
+function resetTable() {
+  gameMenu.classList.remove('hide')
+  while(gameContainer.firstChild) {
+    gameContainer.removeChild(gameContainer.firstChild);
+  }
+}
+
+function getRandomPosition(minPos, diff) {
+  return Math.floor(Math.random() * (diff - minPos)) + minPos;
+}
+
+function setBug(minPos, diff) {
+  const bugPosition = getRandomPosition(minPos, diff)
+  const gameCards = document.querySelectorAll('#game-container div')
+  gameCards[bugPosition].classList.add('bug')
+}
+
+startButton.addEventListener('click', () => {
+  resetTable()
+  createTable(difficult)
+  setBug(minimalPosition, difficult)
+  const ArrIntoBugFind = document.querySelectorAll('#game-container div')
+  ArrIntoBugFind.forEach((elem) => {
+    elem.addEventListener('click', handlerForUserFindBug)
+  })
+  gameMenu.classList.add('hide')
+})
+
+
+
+
